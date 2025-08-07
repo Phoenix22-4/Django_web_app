c1015ad8cc1fcc564727e3500962a3fee6d3c761
Phoenix <htmlfirephoenix@gmail.com>
2025-08-07 08:39:36 +0300

    Temporarily disable MQTT listener for setup

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -1,19 +1,19 @@
-# dashboard/apps.py (RUN MODE - FINAL VERSION)
+# dashboard/apps.py
+
 from django.apps import AppConfig
 import os
 
 class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
-    mqtt_client_started = False
 
     def ready(self):
-        # This code will now start the listener when you run the Daphne server.
-        if not self.mqtt_client_started:
-            from .consumers import get_mqtt_client
-            
-            print("--- Starting MQTT client from AppConfig ---")
-            mqtt_client = get_mqtt_client()
-            mqtt_client.start()
-            
-            self.mqtt_client_started = True
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            return
+
+        from .consumers import get_mqtt_client
+        
+        mqtt_client = get_mqtt_client()
+        
+        # This line is now temporarily disabled
+        # mqtt_client.start()
